cc2d1a1a54af4a679be5892c8a97b63b2266872a
Sushant-Luitel <sushantluitel39@gmail.com>
2024-01-31 15:22:20 +0545

    redesigned navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="navbar">Navbar</div>
+      <div className="navbar">
+        <div className="left">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Services</a>
+          <button className="menu-button">Open</button>
+        </div>
+        <div className="right">
+          <input type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+      </div>
     </div>
   );
 }
