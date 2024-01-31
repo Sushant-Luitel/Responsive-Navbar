ceeae6da3df386d3e327ae932a8a19e87283d14e
Sushant-Luitel <sushantluitel39@gmail.com>
2024-01-31 17:41:52 +0545

    hamburger menu with funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import { MenuOutlined } from "@ant-design/icons";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="App">
       <div className="navbar">
-        <div className="left">
+        <button className="menu-button" onClick={() => setShowMenu(!showMenu)}>
+          <MenuOutlined />
+        </button>
+        <div className="left" id={showMenu ? "hidden" : ""}>
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Contact</a>
           <a href="#">Services</a>
-          <button className="menu-button">Open</button>
         </div>
+
         <div className="right">
           <input type="text" placeholder="Search..." />
           <button>Search</button>
